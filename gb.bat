f225c839e341b48235e6c26e21480d2dadf3c7c8
VonC <vonc@laposte.net>
2015-06-14 22:31:29 +0200

    gb.bat: add bump or version as parameter

--- gb.bat
+++ gb.bat
@@ -7,6 +7,15 @@ rem cd
 rem set GO
 %GOROOT%\bin\go.exe install
 set PATH=.;%PATH%
-git updver
+set a=%1
+if NOT "%a%" == "" (
+	if NOT "%a%" == "bump" (
+		git tag -m "%a% release" %a%
+		set a=bump
+	)
+)
+if "%a%" == "bump" (
+  git updver
+)
 endlocal
 doskey ghref=%~dp0\bin\ghref.exe $*
