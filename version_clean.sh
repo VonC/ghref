365436b1d4fd6cf64adaa3a1daaf9505c0dbf493
VonC <vonc@laposte.net>
2015-06-14 22:03:51 +0200

    version_clean.sh: modify version.go when needed

--- version_clean.sh
+++ version_clean.sh
@@ -4,5 +4,5 @@ git update-index --assume-unchanged -- version.go
 n=$(git ls-files -m|wc -l)
 git update-index --no-assume-unchanged -- version.go
 if [[ ${n} -eq 0 ]]; then
-  sed -e "s;\"[^\"]*\?\";\"${v}\";g" version.go
+  sed -i "s;\"[^\"]*\?\";\"${v}\";g" version.go
 fi
