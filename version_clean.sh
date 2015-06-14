56573c1b4af575d8e87c26d211c75830edfe6ad8
VonC <vonc@laposte.net>
2015-06-14 22:00:55 +0200

    Remove version filter

--- version_clean.sh
+++ version_clean.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 v=$(git describe --all --long --abbrev=7)
-git update-index --assume-unchanged -- $1
+git update-index --assume-unchanged -- version.go
 n=$(git ls-files -m|wc -l)
-git update-index --no-assume-unchanged -- $1
-if [[ ${n} -gt 0 ]]; then
-  sed -e "s;xx\";\";g"
-else
-  sed -e "s;\"[^\"]*\?\";\"${v}\";g"
+git update-index --no-assume-unchanged -- version.go
+if [[ ${n} -eq 0 ]]; then
+  sed -e "s;\"[^\"]*\?\";\"${v}\";g" version.go
 fi
